ee6d989cc39b87e809bdf0528735608dc4ca40e4
NoahEliasson <noah7520@outlook.com>
2023-12-07 19:49:53 +0100

    second commit SQL
    försök att koppla till nu till min databas genom mysql data dependencies i IDE:n.

--- DatabaseSQLMusicApp/AlbumsDOA.cs
+++ DatabaseSQLMusicApp/AlbumsDOA.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,19 @@ namespace DatabaseSQLMusicApp
 {
     public class AlbumsDOA
     {
-        public List<Album> albums = new List<Album>();
+        string connectionString ="test"; //vi pratar med vår lokala server vi öppnat genom MAMP
 
+        public List<Album> getAllAlbums()
+        {
+            //starta med en tom lista, Vi queryar till info i nedan lista och returnar sedan dessa till listan 
+            List<Album> returnThese = new List<Album>();
 
+            //koppla ihop till mySQL server, vi använder inte SQL Server. Vi använder MySQL
+
+            MySqlConnection connection = new MySqlConnection(connectionString); //koppla till din server 
+            connection.Open();
+
+            return returnThese;
+        }
     }
 }
--- DatabaseSQLMusicApp/Form1.cs
+++ DatabaseSQLMusicApp/Form1.cs
@@ -12,30 +12,10 @@ namespace DatabaseSQLMusicApp
         {
             AlbumsDOA albumsDOA = new AlbumsDOA();
             
-            Album a1 = new Album
-            {
-                ID = 1,
-                AlbumName = "my first album",
-                ArtistName = "Noah Eliasson",
-                Year = 2023,
-                ImageURL = "nothing",
-                Description = "nothing special"
-            };
-            Album a2 = new Album
-            {
-                ID = 1,
-                AlbumName = "my second album",
-                ArtistName = "Noah Eliasson",
-                Year = 2023,
-                ImageURL = "nothing",
-                Description = "nothing special"
-            };
-
-            albumsDOA.albums.Add(a1);
-            albumsDOA.albums.Add(a2);
+            
 
             //koppla list och grid view controll
-            albumbBindingSource.DataSource = albumsDOA.albums;
+            albumbBindingSource.DataSource = albumsDOA.getAllAlbums;
 
             dataGridView1.DataSource = albumbBindingSource;
         }
