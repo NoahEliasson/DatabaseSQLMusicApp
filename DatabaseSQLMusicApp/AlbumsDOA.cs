f0148266dfb8087e28ab4fbdfab6edda31ea5eb3
NoahEliasson <noah7520@outlook.com>
2023-12-08 15:11:01 +0100

    Update AlbumsDOA.cs

--- DatabaseSQLMusicApp/AlbumsDOA.cs
+++ DatabaseSQLMusicApp/AlbumsDOA.cs
@@ -9,7 +9,7 @@ namespace DatabaseSQLMusicApp
 {
     public class AlbumsDOA
     {
-        string connectionString ="test"; //vi pratar med vår lokala server vi öppnat genom MAMP
+        string connectionString = "datasource=localhost;port=3306;username=root;password=root;database=music;"; //vi pratar med vår lokala server vi öppnat genom MAMP
 
         public List<Album> getAllAlbums()
         {
@@ -19,7 +19,27 @@ namespace DatabaseSQLMusicApp
             //koppla ihop till mySQL server, vi använder inte SQL Server. Vi använder MySQL
 
             MySqlConnection connection = new MySqlConnection(connectionString); //koppla till din server 
-            connection.Open();
+            
+
+           // MySqlCommand command = new MySqlCommand("SELECT * FROM albums;");
+
+            ///using (MySqlDataReader reader = command.ExecuteReader())
+            //{
+               // while (reader.Read())
+               // {
+                    //Album a = new Album();
+                   // {
+                      //  ID = reader.GetInt32(0),
+                     //   AlbumName = reader.GetString(1),
+                     //  ArtistName = reader.GetString(2),
+                     //   Year = reader.GetInt32(3),
+                     //   ImageURL = reader.GetString(4),
+                   //     Description = reader.GetString(5)
+                 //   };
+               //     returnThese.Add(a);
+             //   }
+           /// }
+
 
             return returnThese;
         }
